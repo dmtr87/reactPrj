cc6f8af3723027476371f54b4cd45ce66c3a3196
Dmitry Turubchuk <exe.dmtr@gmail.com>
2022-03-17 14:14:12 +0300

    add compotetn App

--- src/index.js
+++ src/index.js
@@ -1,39 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import App from './App';
+import App from './components/app/';
 
-const Header = () => {
-  return <h2>Hello World</h2>
-}
-const Field = () => {
-  return <input type="type" placeholder="type here"/>
-}
-const Btn = () => {
-
-  const text = "Log In"
-
-  const res = () => {
-    return "Log In, pl"
-  }
-
-  const logger = false;
-
-  return <button>{logger ? "Enter": text}</button>
-
-}
-
-const App = () => {
-  return (
-    <div>
-      <Header/>
-      <Field/>
-      <Btn/>
-  </div>
-  )
-
-}
-
-
-ReactDOM.render(<App/>,document.getElementById('root')
-);
+ReactDOM.render(<App />,document.getElementById('root'));
