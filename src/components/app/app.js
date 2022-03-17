8c1d1dbfe0ef2b6d2d42733d22773899c8428a3a
Dmitry Turubchuk <exe.dmtr@gmail.com>
2022-03-17 15:06:37 +0300

    added search-panel components

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,11 +1,20 @@
 import React from 'react'; 
 import AppHeader from '../app-header';
+import SearchPanel from '../search-panel';
 
 
 
 
 const App = () => {
-    return (<AppHeader/>)
+    return (
+        <div className='app'>
+            <AppHeader/>
+            <div className='search-panel d-flex'>
+                <SearchPanel/>
+            </div>
+        </div>
+    
+    )
 };
 
 export default App;
---
+++ src/components/search-panel/search-panel.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const SearchPanel = () => {
+    return (
+        <input
+            className="form-control search-imput"
+            type="text"
+            placeholder="Поиск по записям"
+
+        />
+        )
+}
+
+export default SearchPanel;
