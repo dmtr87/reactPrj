f540104ae303570a95259aed1e59fe1cc10e0996
Dmitry Turubchuk <exe.dmtr@gmail.com>
2022-03-21 21:50:09 +0300

    add style for components

--- src/components/post-list-item/post-list-item.js
+++ src/components/post-list-item/post-list-item.js
@@ -1,10 +1,11 @@
 import React from "react";
+import "./post-list-item.css";
 
 
 const PostListItem = () => {
     return (
         <li className="app-list-item  d-flex justify-content-between">
-            <span className="App-list-item-label">
+            <span className="app-list-item-label">
                 Мысли вслух
             </span>
             <div className="d-flex justify-content-center align-items-center">
