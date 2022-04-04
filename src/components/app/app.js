8b8a6e3000e28e600aeb89bed8a63c86623889e5
Dmitry Turubchuk <exe.dmtr@gmail.com>
2022-04-04 22:01:20 +0300

    Add bootstrap,reactstrap, styled-components

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -5,6 +5,13 @@ import PostStatusFilter from '../post-status-filter';
 import PostList from '../post-list';
 import PostAddForm from '../post-add-form';
 import "./app.css";
+import styled from 'styled-components';
+
+
+const AppBlock= styled.div`
+    margin: 0 auto;
+    max-width: 800px;
+`
 
 
 
@@ -18,7 +25,7 @@ const App = () => {
     ]
 
     return (
-        <div className='app'>
+        <AppBlock>
             <AppHeader/>
             <div className='search-panel d-flex'>
                 <SearchPanel/>
@@ -26,7 +33,7 @@ const App = () => {
             </div>
             <PostList data={dataBase}/>
             <PostAddForm/>
-        </div>
+        </AppBlock>
     
     )
 };
--- src/components/post-status-filter/post-status-filter.js
+++ src/components/post-status-filter/post-status-filter.js
@@ -1,9 +1,11 @@
 import React from "react";
+import { Button } from 'reactstrap';
 
 const PostStatusFilter = () => {
     return (
         <div className="btn-group">
-            <button type='button' className='btn btn-info'>Всё</button>
+            <Button color="info" outline>Всё</Button>
+            {/* <button type='button' className='btn btn-info'>Всё</button> */}
             <button type='button' className='btn btn-outline-secondary'>Понравилось</button>
         </div>
     )
